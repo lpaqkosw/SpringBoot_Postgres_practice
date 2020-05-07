dcde5d1cc78f0546ddeab743965c537a7652545a
lpaqkosw <jaehoonlee1994@gmail.com>
2020-05-07 20:32:08 +0900

    erased

--- server/muidb/src/main/java/muidb/muidb/DemoApplication.java
+++ server/muidb/src/main/java/muidb/muidb/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.setProperty("aws.accessKeyId", "AKIAU5757V3AIOV2TBOD");
-		System.setProperty("aws.secretKey", "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");
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
